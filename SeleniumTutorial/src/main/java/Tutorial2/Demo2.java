735982651d9a284cf091d78d174fb7a7569ea0e9
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-07-31 21:08:56 +0530

    window management

---
+++ SeleniumTutorial/src/main/java/Tutorial2/Demo2.java
@@ -0,0 +1,21 @@
+package Tutorial2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo2 {
+	public static void main(String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		
+		driver.switchTo().newWindow(WindowType.TAB);
+		
+		driver.get("https://www.facebook.com");
+		
+		driver.quit();
+	}
+
+}
