842773a69614e3a606dbd32a5956fbbcf06fff2d
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-07 21:48:34 +0530

    Create Demo1.java

---
+++ SeleniumTutorial/src/main/java/Tutorial7/Demo1.java
@@ -0,0 +1,22 @@
+package Tutorial7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+	public static void main(String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://prafpawar11.github.io/mainFrame.html");
+		
+		driver.findElement(By.id("name")).sendKeys("Pawar");
+		
+		driver.switchTo().frame("mainframe");
+		
+		driver.findElement(By.id("Selenium")).click();
+		
+	}
+
+}
