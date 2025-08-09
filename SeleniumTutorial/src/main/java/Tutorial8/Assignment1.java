cc0b550e49f8cd0bfaeeb1f3513c5a12b1b99f76
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-09 23:41:09 +0530

    Update Assignment1.java

--- SeleniumTutorial/src/main/java/Tutorial8/Assignment1.java
+++ SeleniumTutorial/src/main/java/Tutorial8/Assignment1.java
@@ -1,16 +1,55 @@
 package Tutorial8;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Assignment1 {
-	public static void main(String[]args) {
+	public static void main(String[]args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.hyrtutorials.com/p/frames-practice.html");
+		
+		WebElement wb = driver.findElement(By.id("name"));
+		wb.sendKeys("Main Frame");
+		
+		driver.switchTo().frame("frm1");
+		
+		System.out.println("Switched to frame 1");
+		
+		Thread.sleep(3000);
+		
+		WebElement menu = driver.findElement(By.cssSelector("select[id='selectnav2']"));
+		
+		Select menusel = new Select(menu);
+		
+		menusel.selectByVisibleText("Contact");
+		
+		System.out.println("Contact selected");
+		
+		WebElement contact = driver.findElement(By.cssSelector("#selectnav1"));
+		Select contactsel = new Select(contact);
+		contactsel.selectByVisibleText("- Alerts");
+		
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("#alertBox")).click();
+		
+		Alert alt = driver.switchTo().alert();
+		String a = alt.getText();
+		System.out.println(a);
+		alt.accept();
+		
+		
+		
+		
+		
+		
 	}
 
 }
