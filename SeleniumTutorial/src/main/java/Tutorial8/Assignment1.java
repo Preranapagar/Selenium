60bc95c2c99d94a0ea3151329839e0a84f2830b4
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-09 22:18:39 +0530

    Update Assignment1.java

--- SeleniumTutorial/src/main/java/Tutorial8/Assignment1.java
+++ SeleniumTutorial/src/main/java/Tutorial8/Assignment1.java
@@ -1,5 +1,16 @@
 package Tutorial8;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assignment1 {
+	public static void main(String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.hyrtutorials.com/p/frames-practice.html");
+	}
 
 }
