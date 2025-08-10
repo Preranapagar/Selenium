f6b93569797f6e6f307fd7242a1c1ff3b985369f
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-10 14:16:36 +0530

    Create Assignment2.java

---
+++ SeleniumTutorial/src/main/java/Tutorial8/Assignment2.java
@@ -0,0 +1,39 @@
+package Tutorial8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment2 {
+	public static void main(String[]args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://demo.automationtesting.in/Frames.html");
+		
+		driver.switchTo().frame("singleframe");
+		System.out.println("Frame switched");
+		
+		driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Success");
+		
+		Thread.sleep(2000);
+		driver.switchTo().defaultContent();
+		System.out.println("Frame switched to default");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Iframe with in an Iframe']")).click();
+		
+		driver.switchTo().frame(0);
+		System.out.println("Frame switched to Nested Parent");
+		
+		driver.switchTo().frame(0);
+		System.out.println("Frame switched to Nested Child");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Another Success");
+		
+		
+		
+	}
+
+}
