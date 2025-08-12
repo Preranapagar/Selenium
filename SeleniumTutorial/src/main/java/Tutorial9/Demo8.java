b2a5812fda1e771f7e66d264037d289b24f2d675
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-12 23:03:32 +0530

    Update Demo8.java

--- SeleniumTutorial/src/main/java/Tutorial9/Demo8.java
+++ SeleniumTutorial/src/main/java/Tutorial9/Demo8.java
@@ -1,16 +1,27 @@
 package Tutorial9;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Demo8 {
-	public static void maind(String[]args) {
+	public static void main(String[]args) {
 		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.redbus.in");
+		
+		WebElement wb = driver.findElement(By.linkText("Peru"));
+		
+		Actions act = new Actions(driver);
+		
+		act.scrollToElement(wb).build().perform();
+		
+		
 	}
 
 }
