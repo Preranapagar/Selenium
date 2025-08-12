ac408b4e7db116f020d50ddf09f213962473da1b
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-12 21:03:10 +0530

    Update Demo1.java

--- SeleniumTutorial/src/main/java/Tutorial9/Demo1.java
+++ SeleniumTutorial/src/main/java/Tutorial9/Demo1.java
@@ -1,5 +1,24 @@
 package Tutorial9;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Demo1 {
+	public static void main(String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/automation-practice-form");
+		
+		WebElement wb = driver.findElement(By.xpath("//input[@id='gender-radio-2']"));
+		
+		Actions act = new Actions(driver);
+		
+		act.click(wb).build().perform();
+		
+	}
 
 }
