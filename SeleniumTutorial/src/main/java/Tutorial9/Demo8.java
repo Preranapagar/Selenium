d7f5ba2b0d15db39851f6e7bdd6801615d7e3338
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-12 22:49:20 +0530

    Create Demo8.java

---
+++ SeleniumTutorial/src/main/java/Tutorial9/Demo8.java
@@ -0,0 +1,16 @@
+package Tutorial9;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo8 {
+	public static void maind(String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.redbus.in");
+	}
+
+}
