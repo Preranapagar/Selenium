846009a63b5be06eeee017dbac864dc150986cc4
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-13 19:39:28 +0530

    Update Assignment1.java

--- SeleniumTutorial/src/main/java/Tutorial9/Assignment1.java
+++ SeleniumTutorial/src/main/java/Tutorial9/Assignment1.java
@@ -4,10 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Assignment1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
 		
@@ -16,6 +17,15 @@ public class Assignment1 {
 		driver.get("https://www.flipkart.com/");
 		
 		WebElement wb = driver.findElement(By.cssSelector("a[aria-label='Electronics']"));
+		
+		Actions act = new Actions(driver);
+		
+		act.moveToElement(wb).build().perform();
+		
+		Thread.sleep(3000);
+		
+		
+		
 	}
 
 }
