5b8f9a2686596c41d3f78c10f49c3df18e5349c2
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-13 07:03:09 +0530

    Create Demo1.java

---
+++ SeleniumTutorial/src/main/java/Tutorial12/Demo1.java
@@ -0,0 +1,15 @@
+package Tutorial12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+	public static void main(String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+	}
+
+}
