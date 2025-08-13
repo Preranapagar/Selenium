1cf7d41facd27bb8966ecd8cc6815df0a43c8c18
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-13 07:03:14 +0530

    Update Assignment1.java

--- SeleniumTutorial/src/main/java/Tutorial9/Assignment1.java
+++ SeleniumTutorial/src/main/java/Tutorial9/Assignment1.java
@@ -8,6 +8,10 @@ public class Assignment1 {
 	public static void main(String[] args) {
 
 		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.flipkart.com/");
 	}
 
 }
