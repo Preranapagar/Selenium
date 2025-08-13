1d7168980dda5db8d3be7da6496aaf0c1cff6c12
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-13 19:15:10 +0530

    Update Assignment1.java

--- SeleniumTutorial/src/main/java/Tutorial9/Assignment1.java
+++ SeleniumTutorial/src/main/java/Tutorial9/Assignment1.java
@@ -1,6 +1,8 @@
 package Tutorial9;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Assignment1 {
@@ -12,6 +14,8 @@ public class Assignment1 {
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.flipkart.com/");
+		
+		WebElement wb = driver.findElement(By.cssSelector("a[aria-label='Electronics']"));
 	}
 
 }
