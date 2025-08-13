5abe2f44477b85e7b2917f3c8bbd6f491b2d3f6a
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-13 19:15:06 +0530

    Update Demo1.java

--- SeleniumTutorial/src/main/java/Tutorial12/Demo1.java
+++ SeleniumTutorial/src/main/java/Tutorial12/Demo1.java
@@ -1,6 +1,8 @@
 package Tutorial12;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo1 {
@@ -10,6 +12,17 @@ public class Demo1 {
 		
 		driver.manage().window().maximize();
 		
+		driver.get("https://www.flipkart.com/");
+		
+		driver.findElement(By.xpath("//input[@class='Pke_EE']")).sendKeys("Samsumg s24 ultra");
+		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		WebElement wb = driver.findElement(By.xpath("//div[@data-id='MOBH3P4UBUYFSDWV']/descendant::div[@class='Nx9bqj _4b5DiR']"));
+
+		String price = wb.getText();
+		
+		System.out.println(price);
 	}
 
 }
