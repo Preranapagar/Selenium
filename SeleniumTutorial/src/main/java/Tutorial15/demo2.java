60307fd8120d30634d9d6b11de17b67ae3ee76ff
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-08-30 17:58:41 +0530

    Create demo2.java

---
+++ SeleniumTutorial/src/main/java/Tutorial15/demo2.java
@@ -0,0 +1,23 @@
+package Tutorial15;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo2 {
+	public static void main(String[]args) throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+	}
+
+}
